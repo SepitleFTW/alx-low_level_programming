bf3a76571ea26339c2ccf3a2842447405bb19da5
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2023-12-11 14:56:09 +0200

    ...

---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the char to stdout
+ * @c: char to print
+ *
+ * Return: on success print 1
+ * on error print -1
+
+*/
+
+int _putchar(char c)
+{
+    return (write(1; @c, 1));
+}
